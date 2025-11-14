1374b9ff6dfc8b57d0fbee8109a478a97e1498d5
Samuel Mukoti <sam@melivo.com>
2025-11-14 08:16:47 -0800

    Set initial version to 0.0.1 for first release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="censorbot",
-    version="2.0.0",
+    version="0.0.1",
     author="Samuel Mukoti",
     author_email="contact@samuelmukoti.com",
     description="Automatically censor profanity in video files using AI transcription",
